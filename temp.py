aad7f73ad91d4267804f7829d890c28b0f5c8abe
shivam5522 <shivamthakkar36@gmail.com>
2022-01-24 22:55:26 +0530

    Tumhara kaam karliya besharmo

--- temp.py
+++ temp.py
@@ -1,9 +1,24 @@
-from nltk.corpus import treebank_chunk
-from nltk.chunk import RegexpParser
-from chunkers import sub_leaves
-  
-from chunkers import PersonChunker
-chunker = PersonChunker()
-print ("Person name  : ", 
-       sub_leaves(chunker.parse(
-               treebank_chunk.tagged_sents()[0]), 'PERSON'))
+from selenium import webdriver
+import time
+
+driver=webdriver.Chrome()
+
+driver.get('https://tecoholic.github.io/ner-annotator/')
+driver.maximize_window()
+
+# Loop here
+
+driver.find_element_by_xpath('//input[@name="textfile"]').send_keys(r'C:\Users\Shivam Thakkar\Desktop\Coding\Deep Blue\1.txt')
+
+driver.find_element_by_xpath('//select[@name="split_type"]').click()
+
+driver.find_element_by_xpath('//option[@value="emptyline"]').click()
+
+l=['PERSON','LOCATION','SKILLS','CONTACT','EMAIL','ROLE','ORGANISATION','DURATION','CERTIFICATE','PROJECTS','EDUCATION','INSTITUTION']
+
+for i in l:
+        driver.find_element_by_xpath('//input[@type="text"]').send_keys(i)
+        driver.find_element_by_xpath('//button[contains(text(),"Add")]').click()
+        driver.find_element_by_xpath('//span[@class="icon"]').click()
+
+time.sleep(300)
